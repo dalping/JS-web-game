64f56fca4d9e5ca563912f40c96049f979b44275
Dalping <kbing14@naver.com>
2021-09-16 03:30:11 +0900

    .

--- card match game/index.js
+++ card match game/index.js
@@ -1,30 +1,49 @@
 
 let arr = []
-let card_val = []
-let pick_card = '-1';
-const $btn = document.querySelector('.btn');
+const card_val = []
+var pick_card = '-1';
+let result = 0;
+let colors = []
 
-const onCardHandler = (e) =>{
+const $btn = document.querySelector('.btn');
+const $cards = document.querySelectorAll(".card")
+
+const onCardHandler = (e) => {
     if(pick_card !== '-1'){
-        console.log(pick_card.innerText)
-        if(String(pick_card.innerText) === String(e.target.value)){
-            console.log('hello')
-            pick_card.style.backgroundColor = 'white';
-            pick_card.style.border = '1px solid white';
-            e.target.style.backgroundColor = 'white';
+    
+        if(e.target === pick_card) return //자기 자신을 선택할 시 스킵
+
+        if(pick_card.innerText === e.target.innerText){ //맞음
+            onCardClear(pick_card);
+            onCardClear(e.target);
+            pick_card = '-1';
+        } else { //틀림
+            e.target.style.backgroundColor = 'pink';
+            setTimeout(()=>{
+                e.target.style.backgroundColor = 'black';
+                pick_card.style.backgroundColor = 'black'; //머임 왜 안돼
+                pick_card = '-1';
+            },500)
         }
-        pick_card.style.backgroundColor = 'pink';
-        pick_card = '-1';
-        //pick_card 비우기
+        
     }else{ //pick_card가 비어있음
         pick_card = e.target;
-        e.target.style.backgroundColor = 'red';
+        e.target.style.backgroundColor = 'pink';
     }
 }
 
-const $cards = document.querySelectorAll(".card")
+const onCardClear = (card) => {
+    card.style.backgroundColor = 'white';
+    card.style.border = '1px solid white';
+    card.textContent = '';
+    card.removeEventListener('click',onCardHandler);
+    card.style.cursor = 'default';
+}
 
 $btn.addEventListener("click",(e)=>{
+
+    startGame()
+
     for(let i=0; i<16; i++){
         $cards[i].style.backgroundColor = "pink";
      }
@@ -32,18 +51,21 @@ $btn.addEventListener("click",(e)=>{
     setTimeout(()=>{
         for(let i=0; i<16; i++){
            $cards[i].style.backgroundColor = "black";
+           $cards[i].addEventListener("click", onCardHandler);
         }
     },3000);
 })
 
-for(let i=0 ; i<2 ; i++){
-    arr = [0,1,2,3,4,5,6,7]
-    for(let j=0 ; j<8 ; j++){
-        let random = [Math.floor(Math.random() * arr.length - 1)]
-        let val = arr.splice(random,1)
-        card_val.push(val)
-        $cards[j+(i*8)].textContent = `${val}`
-        $cards[j+(i*8)].addEventListener("click", onCardHandler);
+const startGame = () =>{
+    for(let i=0 ; i<2 ; i++){
+        arr = [0,1,2,3,4,5,6,7]
+        color = ["red","orange","yellow","green","blue","pink","pupple"]
+        for(let j=0 ; j<8 ; j++){
+            let random = [Math.floor(Math.random() * arr.length - 1)]
+            let val = arr.splice(random,1)
+            card_val.push(val)
+            $cards[j+(i*8)].textContent = `${val}`
+        }
     }
 }
 
